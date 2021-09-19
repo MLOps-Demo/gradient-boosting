d2011d1c02e4f0776af0daa9f06cae4b6843d385
shreyassks <skshreyas714@gmail.com>
2021-09-19 11:47:17 +0530

    recent commit

--- src/normalize.py
+++ src/normalize.py
@@ -2,16 +2,20 @@
 Standard Scaling the raw data
 """
 from sklearn.preprocessing import StandardScaler
+from mltrace import create_component, register
 import numpy as np
 import pickle
 
 
+@register(
+    component_name="Pre-Processing", input_vars=["filename"], output_vars=["clean_version"]
+)
 def normalize():
     print("Normalizing the data")
 
     print("Loading split data")
-    x_train = np.load("data/processed_data/x_train.npy")
-    x_test = np.load("data/processed_data/x_test.npy")
+    x_train = np.load("../data/processed_data/x_train.npy")
+    x_test = np.load("../data/processed_data/x_test.npy")
     print("done")
 
     print("Scaling data with Standard Scaler")
@@ -19,9 +23,16 @@ def normalize():
     scaling.fit(x_train)
     print("done")
 
-    with open("data/scaling_model.pkl", "wb") as x_f:
+    with open("../data/scaling_model.pkl", "wb") as x_f:
         pickle.dump(scaling, x_f)
 
 
 if __name__ == '__main__':
+    # Create component
+    create_component(
+        name="Pre-Processing",
+        description="Normalizes data",
+        owner="shreyas",
+        tags=["etl"],
+    )
     normalize()
