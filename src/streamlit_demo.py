7023f0cce63f9e0048ac692cace17b77ed608409
shreyassks <skshreyas714@gmail.com>
2021-09-19 10:55:48 +0530

    modified streamlit code

--- src/streamlit_demo.py
+++ src/streamlit_demo.py
@@ -1,5 +1,8 @@
 import git
 import streamlit as st
+import dvc.api
+import pandas as pd
+
 
 REPO = git.Repo(".")
 MODELS_COMMITS = list(REPO.iter_commits(paths="dvc.lock"))
@@ -11,3 +14,15 @@ selected_commit = st.selectbox(
 )
 
 st.write("Selected Commit", selected_commit)
+
+
+@st.cache
+def load_predictions(rev: str) -> pd.DataFrame:
+    with dvc.api.open("data/predictions.csv", rev=rev) as f:
+        return pd.read_csv(f)
+
+
+predictions = load_predictions(rev=selected_commit.hexsha)
+
+st.dataframe(predictions)
+
